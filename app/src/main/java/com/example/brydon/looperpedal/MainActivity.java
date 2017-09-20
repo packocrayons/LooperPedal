2333f580b468b7f4cc075813c2be58391f46bf77
packocrayons <gibson.brydon@gmail.com>
2017-09-20 09:10:07 -0400

    Looks like everything that's currently there is working

--- app/src/main/java/com/example/brydon/looperpedal/MainActivity.java
+++ app/src/main/java/com/example/brydon/looperpedal/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.brydon.looperpedal;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -35,20 +36,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         spareRecorder = new MediaRecorder();
-        System.out.println("setup recorder returned" + setUpRecorder(spareRecorder)); //setup the initial spare recorder
         players.add(new MediaPlayer()); //always keep a spare of these as well.
         baseFilename = getFilesDir().getAbsolutePath() + "/loopRecord";
         System.out.println(baseFilename);
         recordButton = (Button) findViewById(R.id.recordButton);
+        System.out.println("setup recorder returned" + setUpRecorder(spareRecorder)); //setup the initial spare recorder
     }
 
 
     private boolean setUpRecorder(MediaRecorder r){
         r.setAudioSource(MediaRecorder.AudioSource.MIC);
         r.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT); //dont really care
-        r.setOutputFile(baseFilename + (++numLoops));
-        r.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT); //still dont care
+        numLoops += 1;
+        r.setOutputFile(baseFilename + (numLoops));
+        r.setAudioEncoder(MediaRecorder.AudioEncoder.AAC); //still dont care
         try{
+            System.out.println("Next recording will be in file " + baseFilename + numLoops);
             r.prepare();
         } catch(java.io.IOException e) {
             e.printStackTrace();
@@ -67,15 +70,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void restartAll(View view){
-
+        for (int i = 0; i < players.size() - 1; ++i){
+            MediaPlayer p = players.get(i);
+            p.seekTo(0);
+            
+        }
     }
 
     public void clearAll(View view){
-
+        int i = 0; //the first element
+        while(!players.isEmpty()) {
+            MediaPlayer p = players.get(i);
+            p.stop();
+            p.release();
+            players.remove(i);
+        }
     }
 
     public void clearLast(View view){
-
+        MediaPlayer p = players.get(players.size() - 1);
+        p.stop();
+        p.release();
+        players.remove(players.size()-1);
     }
 
     public void toggleRecord(View view){
@@ -83,25 +99,26 @@ public class MainActivity extends AppCompatActivity {
             currentRecorder = spareRecorder; //use our stashed recorder so we don't waste time between button press and actual recording start
             currentRecorder.start();
             recordButton.setText("Recording");
-            //now that we're recording, we can start setting up a new spare recorder in the background
-            spareRecorder = new MediaRecorder();
-            setUpRecorder(spareRecorder);
             recordingFlag = true;
         } else{ //we were recording, stop recording
+            currentRecorder.stop();
+            currentRecorder.release(); //we're done with this one now, we don't need it anymore and we made a new one for next time. This is a spot where this recorder could be updated for a new looper but whatever.
             //first things first, get the track playing as soon as the button is pressed.
             MediaPlayer p = players.get(players.size()-1); //the last mediaPlayer in the list is free to use
             try{
+                System.out.println("Trying to play from " + baseFilename+numLoops);
                 p.setDataSource(baseFilename + numLoops);
             } catch (IOException e){
                 e.printStackTrace();
             }
             p.setLooping(true); //make it loop
             play(p);
+            players.add(new MediaPlayer());
             recordButton.setText("Record");
-            currentRecorder.stop();
-            currentRecorder.release(); //we're done with this one now, we don't need it anymore and we made a new one for next time. This is a spot where this recorder could be updated for a new looper but whatever.
             currentRecorder = null; //can't use it anymore it's been released
-
+            spareRecorder = new MediaRecorder(); //set up a spare recorder
+            setUpRecorder(spareRecorder);
+            recordingFlag = false;
         }
     }
 
